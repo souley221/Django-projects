9c630114d2af51f8f79740196bb5b6d0241a706d
sollysouley <sollysouley@gmail.com>
2017-08-27 15:46:25 -0400

    Add Models : Question, Choice to polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['mysite-sollysouley209402.codeanyapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
